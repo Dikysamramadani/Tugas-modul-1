61dd951ab54e4ba3acdd6293c9f8f1e7c70bfcb8
Dikysamramadani <dikysamramadani333@gmail.com>
2024-12-17 19:51:06 +0700

    Add files via upload

---
+++ modul 5/modul 5/10-Percabangan/main.py
@@ -0,0 +1,29 @@
+# pecarcabangan
+# menggunakan statment IF
+
+# stuktur 
+"""
+    1. if-ya 
+    2. ada kondisi(bernilai TRUE/FLASE)
+    3. ada aksi -> proses lanjutan
+"""
+
+nama = input("ZILDAN")
+
+# IF statement dalam bentuk inline (satu barisan)
+#if nama == "zidan": print (f"selamat datang (zildan)")
+#print("Terima kasih")
+
+# IF statement data bentuk indentasi
+if nama == 'zildan' :
+     print(f'selamat datang (zildan)')
+print("terima kasih") 
+
+# IF-ELASE statement
+if nama == 'zildan' :
+    # aksi 1
+    print(f'kamu(zildan),bukan Weng')
+else:
+    # aksi 2
+    print(f'kamu (zildan),bukan Weng')
+print('terimakasih')
